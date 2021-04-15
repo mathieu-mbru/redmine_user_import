ff48c581a68b3d1d5930b77f69daf65db16fe738
mathieu-mbru <m.brulin@adacis.net>
2021-04-15 11:20:16 +0200

    Correction de override view

--- app/overrides/imports/show.rb
+++ app/overrides/imports/show.rb
@@ -1,6 +1,6 @@
 Deface::Override.new :virtual_path  => 'imports/show',
                      :name          => "Add-view-user-imports",
-                     :insert_after  => "table.list",
+                     :insert_after  => "erb[loud]:contains('_sidebar')",
                      :text          => <<EOF
 <% if User.current.admin?%>
   <p><%= link_to l(:label_user_view_all), users_path(:set_filter => 1, :user_id => @import.saved_objects.map(&:id).join(',')) %></p>
